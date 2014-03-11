09ab01fa3f94e312dcc31a841dc5485f710b5fb0
Outpox <guillaumeoutpox@gmail.com>
2014-03-11 14:18:59 +0100

    Variable player was not declared in the whole function wich lead to errors

--- media/lua/Mods/SwissToolbox/versionVerification.lua
+++ media/lua/Mods/SwissToolbox/versionVerification.lua
@@ -2,15 +2,29 @@ function versionVerificationAuto()
 	getPlayer:Say("SwissToolbox version 0.2 -- Test Barricade");
 end
 
-function versionVerificationMan(_keynum)
+local function versionVerificationMan(_keynum)
 	local key = _keynum;
+	local player = getSpecificPlayer(0);
+	local inv = player:getInventory();
+
 	print (key);
 
+	-- Letter "L"
 	if key == 38 then
-		local player = getSpecificPlayer(0);
+		
 		
 		player:Say("SwissToolbox version 0.2 -- Test Barricade");
 		print("SwissToolbox version 0.2 -- Test Barricade");
+	end
+
+	-- Letter "P"
+	if key == 25 then
+		player:Say("SwissToolbox version 0.2 -- Test Barricade");
+		print("SwissToolbox version 0.2 -- Test Barricade");
+		
+		inv:AddItem("Base.Hammer");
+		inv:AddItem("Base.Saw");
+		inv:AddItem("Base.Screwdriver");
 	end
 end
 
