0c7eea163f7745d5e0aec76e0e7d23648268ca00
Outpox <guillaumeoutpox@gmail.com>
2014-03-11 14:18:08 +0100

    One ";" was missing

--- media/lua/Mods/SwissToolbox/DismantleRecipe.lua
+++ media/lua/Mods/SwissToolbox/DismantleRecipe.lua
@@ -1,6 +1,10 @@
-function dismantleSwissToolbox()
-	if getPlayer():getInventory():contains("SwissToolbox") then
-		getPlayer():getInventory():AddItem("Base.Hammer")
-		getPlayer():getInventory():AddItem("Base.Saw");
-		getPlayer():getInventory():AddItem("Base.Screwdriver");
+function dismantleSwissToolbox(_recipe)
+
+	if _recipe == "Dismantle SwissToolbox" then
+		if getPlayer():getInventory():contains("SwissToolbox") then
+			getPlayer():getInventory():AddItem("Base.Hammer");
+			getPlayer():getInventory():AddItem("Base.Saw");
+			getPlayer():getInventory():AddItem("Base.Screwdriver");
+		end
+	end
 end
