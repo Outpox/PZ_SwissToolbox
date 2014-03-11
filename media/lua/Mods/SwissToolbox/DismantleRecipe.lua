22459a72faf96bff39b8b28bfa448be360813d46
Outpox <guillaumeoutpox@gmail.com>
2014-03-12 00:17:53 +0100

    some debugging text

--- media/lua/Mods/SwissToolbox/DismantleRecipe.lua
+++ media/lua/Mods/SwissToolbox/DismantleRecipe.lua
@@ -1,10 +1,24 @@
-function dismantleSwissToolbox(_recipe)
+function dismantleSwissToolbox(_crafttool, _result, _recipe)
+
+	local player = getSpecificPlayer(0)
+
+	player:Say("Dismantle function called");
+	print("Dismantle function called");
 
 	if _recipe == "Dismantle SwissToolbox" then
+
+		player:Say("Recipe detected");
+		print("Recipe detected");
+
 		if getPlayer():getInventory():contains("SwissToolbox") then
+			player:Say("SwissToolbox detected, adding items");
+			print("SwissToolbox detected, adding items");
+
 			getPlayer():getInventory():AddItem("Base.Hammer");
 			getPlayer():getInventory():AddItem("Base.Saw");
 			getPlayer():getInventory():AddItem("Base.Screwdriver");
 		end
 	end
 end
+
+Events.onMakeItem.Add(dismantleSwissToolbox)
