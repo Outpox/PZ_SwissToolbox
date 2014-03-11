5c6bc0f90410fb507501ff7f033a9b2de41be669
Outpox <guillaumeoutpox@gmail.com>
2014-03-11 13:09:26 +0100

    versionVerificationMan() updated Events done

--- media/lua/Mods/SwissToolbox/versionVerification.lua
+++ media/lua/Mods/SwissToolbox/versionVerification.lua
@@ -2,11 +2,17 @@ function versionVerificationAuto()
 	getPlayer:Say("SwissToolbox version 0.2 -- Test Barricade");
 end
 
-function versionVerificationMan(keynum)
-	if keynum == 38 then
-		getPlayer:Say("SwissToolbox version 0.2 -- Test Barricade");
+function versionVerificationMan(_keynum)
+	local key = _keynum;
+	print (key);
+
+	if key == 38 then
+		local player = getSpecificPlayer(0);
+		
+		player:Say("SwissToolbox version 0.2 -- Test Barricade");
 		print("SwissToolbox version 0.2 -- Test Barricade");
 	end
 end
 
-Events.OnGameStart.Add(versionVerification);
+Events.OnGameStart.Add(versionVerificationAuto);
+Events.OnKeyPressed.Add(versionVerificationMan);
