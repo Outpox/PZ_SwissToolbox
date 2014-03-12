647d73075340a5264d208454b70f2a73bfca6809
Outpox <guillaumeoutpox@gmail.com>
2014-03-12 22:23:47 +0100

    production version (all tests removed)

--- media/lua/Mods/SwissToolbox/versionVerification.lua
+++ media/lua/Mods/SwissToolbox/versionVerification.lua
@@ -1,32 +1,5 @@
 function versionVerificationAuto()
-	getSpecificPlayer(0):Say("SwissToolbox version 0.2 -- Test Barricade");
-end
-
-local function versionVerificationMan(_keynum)
-	local key = _keynum;
-	local player = getSpecificPlayer(0);
-	local inv = player:getInventory();
-
-	print (key);
-
-	-- Letter "L"
-	if key == 38 then
-		
-		
-		player:Say("SwissToolbox version 0.2 -- Test Barricade");
-		print("SwissToolbox version 0.2 -- Test Barricade");
-	end
-
-	-- Letter "P"
-	if key == 25 then
-		player:Say("SwissToolbox version 0.2 -- Test Barricade");
-		print("SwissToolbox version 0.2 -- Test Barricade");
-
-		inv:AddItem("Base.Hammer");
-		inv:AddItem("Base.Saw");
-		inv:AddItem("Base.Screwdriver");
-	end
+	getSpecificPlayer(0):Say("SwissToolbox version 0.3 loaded");
 end
 
 Events.OnGameStart.Add(versionVerificationAuto);
-Events.OnKeyPressed.Add(versionVerificationMan);
