f6fb50d39076433b2966dfc5d569b17952ce0792
Outpox <guillaumeoutpox@gmail.com>
2014-03-12 22:23:10 +0100

    not giving hammer anymore production version

--- media/lua/Mods/SwissToolbox/DismantleRecipe.lua
+++ media/lua/Mods/SwissToolbox/DismantleRecipe.lua
@@ -1,24 +1,15 @@
-function dismantleSwissToolbox(_crafttool, _result, _recipe)
+local function dismantleSwissToolbox(_crafttool, _result, _recipe)
 
-	local player = getSpecificPlayer(0)
+	local player = getSpecificPlayer(0);
 
-	player:Say("Dismantle function called");
-	print("Dismantle function called");
+	if _recipe:getName() == "Dismantle SwissToolbox" then
 
-	if _recipe == "Dismantle SwissToolbox" then
-
-		player:Say("Recipe detected");
-		print("Recipe detected");
-
-		if getPlayer():getInventory():contains("SwissToolbox") then
-			player:Say("SwissToolbox detected, adding items");
-			print("SwissToolbox detected, adding items");
-
-			getPlayer():getInventory():AddItem("Base.Hammer");
+			-- The hammer is given back through the .txt recipe
+			-- getPlayer():getInventory():AddItem("Base.Hammer");
 			getPlayer():getInventory():AddItem("Base.Saw");
 			getPlayer():getInventory():AddItem("Base.Screwdriver");
-		end
 	end
 end
 
-Events.onMakeItem.Add(dismantleSwissToolbox)
+
+Events.OnMakeItem.Add(dismantleSwissToolbox)
